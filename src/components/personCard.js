e7cc6539c38e9ec0678c2f00f905e1b0a1878127
Matthew-Sherman1 <93622077+Matthew-Sherman1@users.noreply.github.com>
2022-01-14 12:48:08 -0700

    Initial commit

---
+++ src/components/personCard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+    
+class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+    birthday= () => {
+        this.setState({age: this.state.age += 1});
+    }
+    render() {
+        return (<div>
+                    <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                    <h3>Age: {this.state.age}</h3>
+                    <h3>Hair Color: {this.props.hairColor}</h3>
+                    <button onClick={this.birthday}>Birthday Button for {this.props.firstName} {this.props.lastName} </button>
+                </div>);
+}
+}
+export default PersonCard;
